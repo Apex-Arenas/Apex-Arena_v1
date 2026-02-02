be7f53745f4fec581dba8c2df300d4c2d502eaca
mighty808 <effahaboagye888@gmail.com>
2026-02-02 23:46:48 +0000

    moved profile to public

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,9 +1,14 @@
 import { Routes, Route, Outlet } from "react-router-dom";
-import Landing from "./pages/Landing";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import SignUp from "./pages/SignUp";
-import Login from "./pages/Login";
+import Register from "./pages/public/register";
+import AdminDashboard from "./pages/auth/admin/dashboard";
+import AdminUsers from "./pages/auth/admin/users";
+import OrganizerProfile from "./pages/public/profile";
+import CreateTournament from "./pages/auth/organizer/create-tournament";
+import JoinTournament from "./pages/auth/player/join-tournament";
+import Landing from "./pages/public/landing";
+import Login from "./pages/public/Login";
 
 const Layout = () => (
   <div className="min-h-dvh flex flex-col bg-white text-gray-800">
@@ -20,8 +25,25 @@ const App = () => {
     <Routes>
       <Route element={<Layout />}>
         <Route index element={<Landing />} />
-        <Route path="signup" element={<SignUp />} />
+        <Route path="signup" element={<Register />} />
+        <Route path="register" element={<Register />} />
         <Route path="login" element={<Login />} />
+
+        <Route path="auth">
+          <Route path="admin">
+            <Route path="dashboard" element={<AdminDashboard />} />
+            <Route path="users" element={<AdminUsers />} />
+          </Route>
+
+          <Route path="organizer">
+            <Route path="profile" element={<OrganizerProfile />} />
+            <Route path="create-tournament" element={<CreateTournament />} />
+          </Route>
+
+          <Route path="player">
+            <Route path="join-tournament" element={<JoinTournament />} />
+          </Route>
+        </Route>
       </Route>
     </Routes>
   );
