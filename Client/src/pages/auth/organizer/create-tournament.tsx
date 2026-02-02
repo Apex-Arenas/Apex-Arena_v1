9217238a192c75ee9dc539256380a124c8e58f6f
mighty808 <effahaboagye888@gmail.com>
2026-02-02 23:46:48 +0000

    changes

---
+++ Client/src/pages/auth/organizer/create-tournament.tsx
@@ -0,0 +1,6 @@
+
+const CreateTournament = () => {
+  return <h1>Create Tournament</h1>;
+};
+
+export default CreateTournament;
---
+++ Client/src/pages/auth/player/join-tournament.tsx
@@ -0,0 +1,6 @@
+
+const JoinTournament = () => {
+  return <h1>Join Tournament</h1>;
+};
+
+export default JoinTournament;
